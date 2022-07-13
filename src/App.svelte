eb2b1c337df8f543f7498b0bd02d119793bd158b
HSWWWW <Helena1980938619@gmail.com>
2022-07-13 00:34:42 -0700

    （・◇・）/~~~ added some formats

--- src/App.svelte
+++ src/App.svelte
@@ -30,52 +30,103 @@
         <div class="column" style="border-top-style: solid;">
           <h3>Beginning --> </h3>
           <p style="position: relative; left: 0px;">First, select a house: <br>I picked the <a href="https://eamesfoundation.org/house/eames-house/">Eames House</a>, a mid-20th century house in Los Angeles.
-            <br>In the next 2 hours, after concluding the 1:200 scale is nearly impossible to work with, I drew out the pattern of one wall.<p/>
+            <br>In the next 2 hours, after concluding the 1:200 scale (1cm = 2m) is nearly impossible to cut out the window grids, I drew out the pattern of one wall.<p/>
           <img src="./images/IMG_2311.webp" width = 600 alt="a single peice of wall">
 
         </div>
         <div class="column" style="border-bottom-style: solid; border-left-style:solid;">
           <h3 style="position: relative; left: 20px">End</h3>
           <p style="position: relative; left: 20px">Final model, scale 1:100</p>
-          <img style="position: relative; left: 20px" src="./images/IMG_2867.webp" width = 600 alt="finished model">
+          <img style="position: relative; left: 20px" src="./images/IMG_2867.webp" width=600 alt="finished model">
         </div>
       </div>
 
     <!-- svelte-ignore a11y-missing-attribute -->
   </section>
+
+
+
+
   <section>
     <h2>1.Conceptual Model</h2>
 
-    <div class="flex-container">
-      <div class="flex-items">
-        <img src="./images/IMG_2313.webp" height = 400 alt="a big hole">
-        <p>I poked a hole too big, using white glue to fill in the space</p>
+    <section class="background">
+      <div class="flex-container">
+        <div>
+          <img class="pic-filler" src="./images/Hicon32.webp" height=400 width =600 alt="space filler">
+          <p style="float: right; position: relative; left: 20px" class="flex-text">summary and idea of first model</p>
+        </div>
+    </section>
+    <br>
+
+    <section class="border">
+    <h4>process</h4>
+    <section>
+      <div class="flex-container">
+        <div class="flex-items">
+          <img src="./images/IMG_2313.webp" height=400 alt="a big hole">
+          <p class="flex-text">I poked a hole too big, using white glue to fill in the space</p>
+        </div>
+        <div class="flex-items">
+          <img src="./images/IMG_2419.webp" height=400 alt="nearly-finished">
+          <p style="font-family: 'Major Mono Display', monospace; color: #d15634;">major update</p>
+          <p>1. With vibrant colors!! Contrast with the (supposedly) clear glass!! 
+          <br> 2. I added the black mullions to make it more structured</p>
+        </div>
       </div>
-      <div class="flex-items">
-        <img src="./images/IMG_2419.webp" height = 400 alt="nearly-finished">
-        <p style="font-family: 'Major Mono Display', monospace; color: #d15634;">major update</p>
-    <p>1. With vibrant colors!! Contrast with the (supposedly) clear glass!! <br>
-  2. I added the black mullions to make it more structured</p>
-      </div>
-    </div>
-
-    
-    
-
-
+    </section>
   </section>
+
   <section>
     <h2>2.Translational Model</h2>
   </section>
 
+  <section class="background">
+    <div class="flex-container">
+      <div>
+        <img class="pic-filler" src="./images/Hicon32.webp" height=400 width =600 alt="space filler">
+        <p style="float: right; position: relative; left: 20px" class="flex-text">summary and idea of second model</p>
+      </div>
+  </section>
+  <br>
+  <section class="border">
+  <h4>process</h4>
+  </section>
 
-  <section>
+
+
     <h2>3.Finish Model</h2>
 
+    <section class="background">
+      <div class="flex-container">
+        <div >
+          <img class="pic-filler" src="./images/Hicon32.webp" height=400 width =600 alt="space filler">
+          <p style="float: right; position: relative; left: 20px" class="flex-text">summary and idea of third model</p>
+        </div>
+    </section>
+    <br>
+    <section>
+    <h4>process</h4>
     <div class="flex-container">
+      <div class="flex-items" >
+        <img src="./images/IMG_2893.webp" height = 400 alt="tools">
+        <p>1. metal wire 
+          <br>2. wooden sticks
+          <br>3. black permanent marker (used to color the wire and the sticks)
+          <br>4. black ink (its much faster to soak the wood sticks in black ink)
+          <br>5. tacky glue (slightly better than white glue)
+          <br>6. ruler
+          <br>7. freshly killed pine leaves
+          <br>8. a thick needle with a handle tool (unclog #9)
+          <br>9. superglue
+          <br>10.tweezer (when your hands are shaky)
+          <br>other: postcard, thin wood plate
+
+        </p>
+      </div>
       <div class="flex-items">
         <img src="./images/IMG_2743.webp" height = 400 alt="a small peice of wall">
-        <p>hi</p>
+        <p>I used wooden sticks </p>
       </div>
       <div class="flex-items">
         <img src="./images/IMG_2744.webp" height = 400 alt="shadows of a wall">
@@ -111,22 +162,22 @@
 
     <div class="flex-container">
       <div class="flex-items">
-        <img src="./images/IMG_2829 2.webp" height = 347 alt="with stair in the middle">
+        <img src="./images/IMG_2829 2.webp" height=335 alt="with stair in the middle">
         <p>hi</p>
       </div>
       <div class="flex-items">
-        <img src="./images/IMG_2834.webp" height = 347 alt="triangle frame balancing on its own">
+        <img src="./images/IMG_2834.webp" height=335 alt="triangle frame balancing on its own">
         <p>hi</p>
       </div>
       <div class="flex-items">
-        <img src="./images/IMG_2840.webp" height = 347 alt="triangle pattern balancing on its own">
+        <img src="./images/IMG_2840.webp" height=335 alt="triangle pattern balancing on its own">
         <p>hi</p>
       </div>
     </div>
-
+  </section>
 
   
-  </section>
+
   
 </main>
 
@@ -138,6 +189,7 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+    
   }
 
   h2 {
@@ -151,16 +203,23 @@
     font-family: 'Major Mono Display', monospace;
     color: #d15634;
     /* text-transform: uppercase; */
-    font-size: 4em;
+    font-size: 3.75em;
     font-weight: 500;
     position: relative;
-    bottom: 30px;
+    bottom: 15px;
+    background: #f5f1ed;
+    padding: 40px
   }
   h3{
     font-family: 'Major Mono Display', monospace;
     font-size: 2em;
-
   }
+
+  h4{
+    font-family: 'Major Mono Display', monospace;
+    font-size: 1.5em;
+  }
+
   p{
     font-family: 'Didact Gothic', sans-serif;
     color: #233b4d;
@@ -174,8 +233,21 @@
     text-align:right;
     padding: 0;
     position:relative;
-    bottom: 30px;
-    left:10px
+    bottom: 0px;
+    left:10px;
+    line-height: normal;
+  }
+  .background{
+    background: #f5f1ed;
+    padding: 10px;
+  }
+  .border{
+    /* border: 2px solid #d5d8e6;
+    padding: 10px; */
+  }
+  .pic-filler{
+    border: 3px solid black;
+    float:left;
   }
   /* Create two equal columns that floats next to each other */
 .column {
@@ -199,9 +271,13 @@
 
 .flex-items{
   padding: 10px; /* this */
+  width: min-content;
 
 }
 
+.flex-text{
+  flex-wrap: wrap;
+}
 @media (min-width: 640px) {
     main {
       max-width: none;
